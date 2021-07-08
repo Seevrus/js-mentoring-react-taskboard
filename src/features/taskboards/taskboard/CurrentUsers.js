e7829b13cadfc04d5794c03f47f728d4a4e82a61
Zoltan Till <Zoltan_Till@epam.com>
2021-07-08 13:31:42 +0200

    Other users accessing a Task Board now have their logged-in status displayed

--- src/features/taskboards/taskboard/CurrentUsers.js
+++ src/features/taskboards/taskboard/CurrentUsers.js
@@ -1,9 +1,10 @@
 import { IoIosClose } from "react-icons/io"
 import { useDispatch, useSelector } from "react-redux"
+import classNames from 'classnames'
 import { selectAllUsers } from "../../users/usersSlice"
 import { removeUserFromBoard } from '../taskBoardsSlice'
 
-const CurrentUser = ({ boardId, userId, userIds, setUserIds, email }) => {
+const CurrentUser = ({ boardId, userId, userIds, setUserIds, email, loggedin }) => {
   const dispatch = useDispatch()
 
   const onCurrentUserRemove = e => {
@@ -14,8 +15,13 @@ const CurrentUser = ({ boardId, userId, userIds, setUserIds, email }) => {
       })
   }
 
+  const userClass = classNames({
+    'current-user': true,
+    'current-user-loggedin': loggedin,
+  })
+
   return (
-    <div className="current-user">
+    <div className={userClass}>
       <span>{email}</span>
       <div className="current-user-remove" onClick={onCurrentUserRemove}><IoIosClose /></div>
     </div>
@@ -27,6 +33,7 @@ export const CurrentUsers = ({ boardId, userIds, setUserIds }) => {
   const filteredUsers = users.filter(user => userIds.includes(user.id))
 
   return (
+    !!filteredUsers.length &&
     <div className="current-users-container">
       Other users currently accessing this board:
       <div className="current-users">
@@ -39,6 +46,7 @@ export const CurrentUsers = ({ boardId, userIds, setUserIds }) => {
               userIds={userIds}
               setUserIds={setUserIds}
               email={user.email}
+              loggedin={user.loggedin}
             />
           )
         })}
